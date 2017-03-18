8765d9d196ff7fea9779e732e9bf168b5f791b0a
DG-PC\DG <dav1dgard1ner@hotmail.com>
2017-03-18 17:42:32 +0000

    3rd commit

--- FileHandling/src/main/java/com/java7/nio/FileHandling/FileSearch.java
+++ FileHandling/src/main/java/com/java7/nio/FileHandling/FileSearch.java
@@ -19,48 +19,48 @@ public class FileSearch {
 
 	public void startFileMonitor() {
 
-		try {
-			WatchService watcher = FileSystems.getDefault().newWatchService();
-			Path dir = FileSystems.getDefault().getPath("data/test");
-
-			WatchKey key = dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
-					StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
-			
-			
-//			handle files currently sitting in directory before polling
-//			what happens if file arrives while sorting these files? start poller first
-
-			try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
-			    for (Path file: stream) {
-			        System.out.println(file.getFileName());
-			    }
-			} catch (IOException | DirectoryIteratorException x) {
-			    System.err.println(x);
-			}
-			
-//			poll for new files that arrive
-			while (!App.shutdown) {
-				System.out.println("searching...");
-				key = watcher.take();
-				for (WatchEvent<?> event : key.pollEvents()) {
-					Path path = (Path) event.context();
-					System.out.println("file name is " + path.toFile().getName());
-					if (path.toFile().getName().equals("shutdown.txt")) {
-						System.out.println("shut down file received...");
-//						shutdown = true;
-						break;
-					}
-				}
-				key.reset();
-			}
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			WatchService watcher = FileSystems.getDefault().newWatchService();
+//			Path dir = FileSystems.getDefault().getPath("data/test");
+//
+//			WatchKey key = dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
+//					StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+//			
+//			
+////			handle files currently sitting in directory before polling
+////			what happens if file arrives while sorting these files? start poller first
+//
+//			try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+//			    for (Path file: stream) {
+//			        System.out.println(file.getFileName());
+//			    }
+//			} catch (IOException | DirectoryIteratorException x) {
+//			    System.err.println(x);
+//			}
+//			
+////			poll for new files that arrive
+//			while (!App.shutdown) {
+//				System.out.println("searching...");
+//				key = watcher.take();
+//				for (WatchEvent<?> event : key.pollEvents()) {
+//					Path path = (Path) event.context();
+//					System.out.println("file name is " + path.toFile().getName());
+//					if (path.toFile().getName().equals("shutdown.txt")) {
+//						System.out.println("shut down file received...");
+////						shutdown = true;
+//						break;
+//					}
+//				}
+//				key.reset();
+//			}
+//
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
 }
