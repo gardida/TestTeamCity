c8fce744386bf3131015c62c1ec531898aeb25a7
DG-PC\DG <dav1dgard1ner@hotmail.com>
2017-03-18 16:24:34 +0000

    changes made

---
+++ FileHandling/src/main/java/com/java7/nio/FileHandling/App.java
@@ -0,0 +1,57 @@
+package com.java7.nio.FileHandling;
+
+import java.lang.management.ManagementFactory;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static volatile boolean shutdown = false;
+	
+	public static void main(String[] args) {
+
+		final String mbeanObjectNameStr = "example:type=ShutDownImpl";
+
+		final MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+		registerMBean(mbs, mbeanObjectNameStr, ShutDownImpl.class);
+
+		FileSearch search = new FileSearch();
+		search.startFileMonitor();
+	}
+
+	public static void registerMBean(final MBeanServer mbs, final String mBeanObjectName, final Class mBeanClass) {
+		try {
+			final ObjectName name = new ObjectName(mBeanObjectName);
+			final Object mBean = mBeanClass.newInstance();
+			mbs.registerMBean(mBean, name);
+		} catch (InstantiationException badInstance) // Class.newInstance()
+		{
+			System.err.println("Unable to instantiate provided class with class " + "name " + mBeanClass.getName()
+					+ ":\n" + badInstance.getMessage());
+		} catch (IllegalAccessException illegalAccess) // Class.newInstance()
+		{
+			System.err.println("Illegal Access trying to instantiate " + mBeanClass.getName() + ":\n"
+					+ illegalAccess.getMessage());
+		} catch (MalformedObjectNameException badObjectName) {
+			System.err.println(mBeanObjectName + " is a bad ObjectName:\n" + badObjectName.getMessage());
+		} catch (InstanceAlreadyExistsException duplicateMBeanInstance) {
+			System.err
+					.println(mBeanObjectName + " already existed as an MBean:\n" + duplicateMBeanInstance.getMessage());
+		} catch (MBeanRegistrationException mbeanRegistrationProblem) {
+			System.err.println(
+					"ERROR trying to register " + mBeanObjectName + ":\n" + mbeanRegistrationProblem.getMessage());
+		} catch (NotCompliantMBeanException badMBean) {
+			System.err.println("ERROR: " + mBeanObjectName + " is not compliant:\n" + badMBean.getMessage());
+		}
+	}
+
+}
---
+++ FileHandling/src/main/java/com/java7/nio/FileHandling/FileSearch.java
@@ -0,0 +1,66 @@
+package com.java7.nio.FileHandling;
+
+import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class FileSearch {
+
+	
+
+	public void startFileMonitor() {
+
+		try {
+			WatchService watcher = FileSystems.getDefault().newWatchService();
+			Path dir = FileSystems.getDefault().getPath("data/test");
+
+			WatchKey key = dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
+					StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+			
+			
+//			handle files currently sitting in directory before polling
+//			what happens if file arrives while sorting these files? start poller first
+
+			try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+			    for (Path file: stream) {
+			        System.out.println(file.getFileName());
+			    }
+			} catch (IOException | DirectoryIteratorException x) {
+			    System.err.println(x);
+			}
+			
+//			poll for new files that arrive
+			while (!App.shutdown) {
+				System.out.println("searching...");
+				key = watcher.take();
+				for (WatchEvent<?> event : key.pollEvents()) {
+					Path path = (Path) event.context();
+					System.out.println("file name is " + path.toFile().getName());
+					if (path.toFile().getName().equals("shutdown.txt")) {
+						System.out.println("shut down file received...");
+//						shutdown = true;
+						break;
+					}
+				}
+				key.reset();
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
