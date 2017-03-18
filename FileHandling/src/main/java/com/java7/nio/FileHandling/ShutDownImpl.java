f95b2fe92cc5911b99ce590596018921bda07ef4
DG-PC\DG <dav1dgard1ner@hotmail.com>
2017-03-18 15:38:25 +0000

    initial commit

---
+++ FileHandling/src/main/java/com/java7/nio/FileHandling/ShutDownImpl.java
@@ -0,0 +1,12 @@
+package com.java7.nio.FileHandling;
+
+public class ShutDownImpl implements ShutDownMXBean {
+
+	@Override
+	public void shutDown() {
+		System.out.println("shutting down...");
+		App.shutdown = true;
+	}
+
+	
+}
---
+++ FileHandling/src/main/java/com/java7/nio/FileHandling/ShutDownMXBean.java
@@ -0,0 +1,7 @@
+package com.java7.nio.FileHandling;
+
+public interface ShutDownMXBean {
+	
+	public void shutDown();
+
+}
